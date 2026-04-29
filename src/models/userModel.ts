10adfaa696dc25a79f88a45b2a25adc401459fdb
fikayo salu <salufikayo@gmail.com>
2026-04-29 01:51:07 +0100

    refactor backend project structure

---
+++ src/models/userModel.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import { uuidv7 } from "uuidv7";
+
+const userSchema = new Schema(
+	{
+		id: {
+			type: Schema.Types.UUID,
+			default: () => uuidv7(),
+		},
+		github_id: {
+			type: String,
+			unique: true,
+		},
+		username: String,
+		email: String,
+		avatar_url: String,
+		role: {
+			type: String,
+			enum: {
+				values: ["admin", "analyst"],
+			},
+			default: "analyst",
+		},
+		is_active: {
+			type: Boolean,
+			default: true,
+		},
+		last_login_at: Date,
+		created_at: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		toJSON: {
+			transform(doc: Document, ret: Record<string, unknown>) {
+				const { _id, __v, id, ...rest } = ret;
+				return { id, ...rest };
+			},
+		},
+	},
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
