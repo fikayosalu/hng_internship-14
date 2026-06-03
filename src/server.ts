708444964d585829d9883b8965d9ab3e3df1090b
fikayo salu <salufikayo@gmail.com>
2026-06-03 09:24:01 +0100

    feat(): add request limit to all routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
  * This module contains the set up for database and express servers
  */
 
-import express, { Request, Response } from "express";
+import express from "express";
 import "dotenv/config";
 import profileRouter from "./routes/profileRouter";
 import cors from "cors";
@@ -10,19 +10,19 @@ import mongoose from "mongoose";
 import "./config/passport";
 import passport from "passport";
 import authRouter from "./routes/authRouter";
+import { limit10, limit60 } from "./utils/limiter";
+import morgan from "morgan";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(cors());
 app.use(passport.initialize());
 
-app.get("/test", (req: Request, res: Response) => {
-	return res.json({ message: "just testing" });
-});
-app.use("/auth", authRouter);
-app.use("/api/profiles", profileRouter);
+app.use("/auth", limit10, authRouter);
+app.use("/api/profiles", limit60, profileRouter);
 
 // --- MongoDB Database Set up ---
 
